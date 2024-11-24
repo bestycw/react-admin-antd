9cd4be42c436a1d261006c7d91e44f4b9434fdb8
yangcw <yangcw@glodon.com>
2024-11-24 15:00:31 +0800

    feat:打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
-  server: {
-    port: 3000
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-antd': ['antd', '@ant-design/icons'],
+          'vendor-editor': ['@wangeditor/editor', '@wangeditor/editor-for-react', '@monaco-editor/react'],
+          'vendor-mobx': ['mobx', 'mobx-react-lite'],
+          'vendor-utils': ['axios', 'dayjs', 'lodash'],
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    },
+    cssCodeSplit: true,
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    chunkSizeWarningLimit: 1000
   }
 })
