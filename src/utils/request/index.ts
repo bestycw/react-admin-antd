f9b857c1ba39021f05476ad7f081cf1f5f1ca1f8
yangcw <yangcw@glodon.com>
2024-12-26 17:44:50 +0800

    feat: 加入mqtt功能

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequest } from './axios';
 import { FetchRequest } from './fetch';
 import { WebSocketRequest, type WebSocketConfig, WebSocketState } from './websocket';
+import { MQTTRequest, type MQTTConfig } from './mqtt';
 
 // 创建默认实例
 const request = new AxiosRequest();
@@ -9,21 +10,26 @@ const fetchRequest = new FetchRequest();
 // WebSocket 工厂函数
 const createWebSocket = (config: WebSocketConfig) => new WebSocketRequest(config);
 
+// MQTT 工厂函数
+const createMQTT = (config: MQTTConfig) => new MQTTRequest(config);
+
 // 统一导出
 export {
   // 类
   AxiosRequest,
   FetchRequest,
   WebSocketRequest,
+  MQTTRequest,
   
   // 实例
   request,
   fetchRequest,
   createWebSocket,
+  createMQTT,
 };
 
 // 类型导出
-export type { WebSocketConfig };
+export type { WebSocketConfig, MQTTConfig };
 export { WebSocketState };
 
 // 默认导出 axios 实例
